a6c927ae3615310369f0f84305a06b14d6df87b0
Stelios Tsesmetzis <stylianos.tsesmetzis@gmail.com>
2025-03-24 01:52:20 +0200

    Add JavaFX hello world

--- src/main/java/com/tseste/personal_finance_analytics/Main.java
+++ src/main/java/com/tseste/personal_finance_analytics/Main.java
@@ -1,8 +1,24 @@
 package com.tseste.personal_finance_analytics;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 300);
+
+        primaryStage.setTitle("Personal finance analytics");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        Budget myBudget = new Budget();
-        myBudget.showBudget();
+        launch(args);
     }
 }
